f2cc692a02d8a5b46fa3bc1e262636897310255c
hithesh23999 <124361222+hithesh23999@users.noreply.github.com>
2023-02-09 23:26:31 +0530

    Update code1.c

--- week 0./code1.c
+++ week 0./code1.c
@@ -1,1 +1,13 @@
 
+DIAGONAL DIFFERENCE
+int diagonalDifference(int r, int c, int** a) {
+    int sum1=0,sum2=0;
+    for(int i=0;i<r;i++)
+{for(int j=0;j<c;j++){
+    if(i==j)
+    { sum1+=a[i][j];
+    }
+if(i+j==r-1)
+{sum2+=a[i][j];}
+}}
+return abs(sum1-sum2);}
